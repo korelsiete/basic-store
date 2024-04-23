8db53535d835290894a184773dc501c3ebbdf011
Stefano Flores <stefanoypf@gmail.com>
2024-04-23 14:47:35 -0500

    show event properties

--- scripts/filterProducts.js
+++ scripts/filterProducts.js
@@ -1,10 +1,13 @@
 const mainSearch = document.querySelector(".container #search");
 const dropdownSearch = document.querySelector(".dropdown-menu #search");
 
-function captureText() {
-  console.log("capturado");
+function captureText(event) {
+  console.log(event);
+  console.log(event.target);
+  console.log(event.key);
+  console.log(event.target.value);
 }
 
 for (const search of [mainSearch, dropdownSearch]) {
-  search.addEventListener("keyup", captureText);
+  search.addEventListener("keyup", (event) => captureText(event));
 }
