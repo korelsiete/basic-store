50fe550a3360526457ad17796d2d9075d514692e
Stefano Flores <stefanoypf@gmail.com>
2024-04-22 19:59:13 -0500

    refactor: render footer from js

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,7 +1,8 @@
 const mainNav = document.querySelector(".container .nav ul");
 const dropdownNav = document.querySelector(".dropdown-menu .nav ul");
+const footerContainer = document.querySelector(".columns-container");
 
-const options = [
+const optionsForNav = [
   { title: "Ofertas", linkTo: "#" },
   { title: "Cómo comprar", linkTo: "#" },
   { title: "Costos y tarifas", linkTo: "#" },
@@ -9,14 +10,48 @@ const options = [
   { title: "Garantía", linkTo: "#" },
 ];
 
+const optionsForFooter = [
+  {
+    opts: [
+      { title: "Ofertas", linkTo: "#", isTitle: true },
+      { title: "Laptop", linkTo: "#" },
+      { title: "Audio", linkTo: "#" },
+      { title: "Auriculares", linkTo: "#" },
+    ],
+  },
+  {
+    opts: [
+      { title: "Cómo comprar", linkTo: "#", isTitle: true },
+      { title: "Formas de pago", linkTo: "#" },
+      { title: "Envíos", linkTo: "#" },
+      { title: "Devoluciones", linkTo: "#" },
+    ],
+  },
+  {
+    opts: [
+      { title: "Costos y tarifas", linkTo: "#", isTitle: true },
+      { title: "Impuestos", linkTo: "#" },
+      { title: "Facturación", linkTo: "#" },
+    ],
+  },
+  {
+    opts: [
+      { title: "Mis pedidos", linkTo: "#", isTitle: true },
+      { title: "Pedir nuevamente", linkTo: "#" },
+      { title: "Lista de deseos", linkTo: "#" },
+    ],
+  },
+  { opts: [{ title: "Garantía", linkTo: "#", isTitle: true }] },
+];
+
+// Loop to add the NAV options
 for (let nav of [mainNav, dropdownNav]) {
-  for (let option of options) {
+  for (let option of optionsForNav) {
     // Create a new list item
     const li = document.createElement("li");
 
     // Create a link and their properties
     const anchor = document.createElement("a");
-    anchor.className = "nav-button";
     anchor.textContent = option.title;
     anchor.href = option.linkTo;
 
@@ -26,4 +61,26 @@ for (let nav of [mainNav, dropdownNav]) {
     // Add the list item to the nav
     nav.appendChild(li);
   }
+}
+
+// Loop to add the FOOTER options
+for (let option of optionsForFooter) {
+  let div = document.createElement("div");
+  div.className = "col";
+  let ul = document.createElement("ul");
+
+  for (let opt of option.opts) {
+    const li = document.createElement("li");
+    if (opt.isTitle) li.className = "col-main-item";
+
+    const a = document.createElement("a");
+    a.textContent = opt.title;
+    a.href = opt.linkTo;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+
+  div.appendChild(ul);
+  footerContainer.append(div);
 }
