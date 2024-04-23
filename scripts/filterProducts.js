5d04131e2a1553c4fa03acec8bda37df24ecd0ed
Stefano Flores <stefanoypf@gmail.com>
2024-04-23 14:44:10 -0500

    event keyboard with eventListener

--- scripts/filterProducts.js
+++ scripts/filterProducts.js
@@ -1,3 +1,10 @@
+const mainSearch = document.querySelector(".container #search");
+const dropdownSearch = document.querySelector(".dropdown-menu #search");
+
 function captureText() {
   console.log("capturado");
+}
+
+for (const search of [mainSearch, dropdownSearch]) {
+  search.addEventListener("keyup", captureText);
 }
